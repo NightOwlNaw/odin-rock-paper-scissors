5b55d3f1844c875f34346b5dcb1915b8d5dabaa2
NightOwlNaw <nightowlnaw@gmail.com>
2026-03-09 12:39:38 +0100

    Add computerChoice logic
    Create function getComputerChoice, with code
    that will randomly set computerChoice to one of 3
    string values (Rock, paper or scissors).

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+// Create function getComputerChoice
+// let computerChoice is undefined
+// Obtain random number between 1 to 100: let randomNumber
+// If randomNumber is between 0 and 33: computerChoice is rock
+// Else if randomNumber is between 33 and 66: computerChoice is paper
+// Else (randomnumber is between 66 and 100): computerChoice is scissors
+
+function getComputerChoice() {
+let computerChoice;
+let randomNumber = Math.random() * 100;
+    console.log(computerChoice);
+    console.log(randomNumber);
+
+if (randomNumber <= 33) {
+    console.log("check 33");
+    computerChoice = "Rock";
+    console.log(computerChoice);
+}
+else if (randomNumber > 33 && randomNumber <= 66) {
+    console.log("check 66");
+    computerChoice = "Paper";
+    console.log(computerChoice);
+}
+else {
+    console.log("higher");
+    computerChoice = "Scissors";
+    console.log(computerChoice);
+}
+}
+
+getComputerChoice();
