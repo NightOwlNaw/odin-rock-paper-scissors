0767d74b6dc1dfdaa86e80ae8d2c9e5bf387d755
NightOwlNaw <nightowlnaw@gmail.com>
2026-03-12 12:22:58 +0100

    Add winner-loser-tie announcements
    Winner-loser-tie announcement are part of the game
    logic. Code ensures ach announcement's first
    letter is capitalized.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,30 +8,30 @@
 function getComputerChoice() {
 let computerChoice;
 let randomNumber = Math.random() * 100;
-    console.log(computerChoice);
-    console.log(randomNumber);
+    // console.log(computerChoice);
+    // console.log(randomNumber);
 
 if (randomNumber <= 33) {
-    console.log("check 33");
-    computerChoice = "Rock";
-    console.log(computerChoice);
+    // console.log("check 33");
+    computerChoice = "rock";
+    // console.log(computerChoice);
     return computerChoice;
 }
 else if (randomNumber > 33 && randomNumber <= 66) {
-    console.log("check 66");
-    computerChoice = "Paper";
-    console.log(computerChoice);
+    // console.log("check 66");
+    computerChoice = "paper";
+    // console.log(computerChoice);
     return computerChoice;
 }
 else {
-    console.log("higher");
-    computerChoice = "Scissors";
-    console.log(computerChoice);
+    // console.log("higher");
+    computerChoice = "scissors";
+    // console.log(computerChoice);
     return computerChoice;
 }
 }
 
-getComputerChoice();
+// getComputerChoice();
 
 
 
@@ -43,15 +43,69 @@ getComputerChoice();
 function getHumanChoice() {
 let humanChoice = prompt("We're playing Rock Paper Scissors, what is you're choice?");
 console.log(humanChoice)
+
 return humanChoice;
 
 }
 
 
-getHumanChoice();
+// getHumanChoice();
+
+
 
 // Create variable humanScore to keep track of the player's score, set to 0.
 // Create variable computerScore to keep track of the computer's score, set to 0.
 
 let humanScore = 0;
 let computerScore = 0;
+
+
+
+// Create function playRound
+// playRound takes two arguments: humanChoice and computerChoice.
+// Make function humanChoice's parameter case-insensitive.
+// Announce round winner in console.log: "You [win]/[lose]! [Winner's choice]
+// beats [loser's choice]." Logic:
+// If humanChoice != computerChoice look at values:
+//    If humanChoice == rock and computerChoice == scissor: user wins
+//    display "You Win! humanChoice"
+//    Else if humanChoice == paper and computerChoice == rock: user wins
+//    Else if humanChoice == scissors and computerChoice == paper: user wins 
+//    Else computer wins
+
+// rock beats scissors 
+// scissors beats paper
+// paper beats rock
+
+
+// Increment humanScore or computerScore variable based on the round winner.
+
+function playRound(humanChoice, computerChoice) {
+humanChoiceLowered = humanChoice.toLowerCase();
+console.log(humanChoiceLowered);
+console.log(computerChoice);
+
+
+console.log(
+"You win! " +
+    humanChoiceLowered.replace(humanChoiceLowered[0], humanChoiceLowered[0].toUpperCase())
++ " beats " + computerChoice + ".");
+
+console.log(
+"You lose! " +
+    computerChoice.replace(computerChoice[0], computerChoice[0].toUpperCase())
++ " beats " + humanChoiceLowered + ".");
+
+console.log(
+    "It's tie! You both chose " + computerChoice + ". Play another round."
+)
+
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
