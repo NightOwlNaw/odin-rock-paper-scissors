0e19ad2a79010112d1a921110f9e46775f043a27
NightOwlNaw <nightowlnaw@gmail.com>
2026-03-12 10:57:14 +0100

    Add user input and keep score
    Add prompt which grabs user's choice in game, to
    be stored as humanChoice.
    
    Add two variables to keep track of scores, one for
    the user's score (humanScore) and one for the com-
    puter's score (computerScore).

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -15,17 +15,43 @@ if (randomNumber <= 33) {
     console.log("check 33");
     computerChoice = "Rock";
     console.log(computerChoice);
+    return computerChoice;
 }
 else if (randomNumber > 33 && randomNumber <= 66) {
     console.log("check 66");
     computerChoice = "Paper";
     console.log(computerChoice);
+    return computerChoice;
 }
 else {
     console.log("higher");
     computerChoice = "Scissors";
     console.log(computerChoice);
+    return computerChoice;
 }
 }
 
 getComputerChoice();
+
+
+
+// Create function getHumanChoice.
+// let humanChoice is undefined.
+// Obtain user input via prompt.
+// Return user input.
+
+function getHumanChoice() {
+let humanChoice = prompt("We're playing Rock Paper Scissors, what is you're choice?");
+console.log(humanChoice)
+return humanChoice;
+
+}
+
+
+getHumanChoice();
+
+// Create variable humanScore to keep track of the player's score, set to 0.
+// Create variable computerScore to keep track of the computer's score, set to 0.
+
+let humanScore = 0;
+let computerScore = 0;
